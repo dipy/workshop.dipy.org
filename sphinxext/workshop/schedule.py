f41ed5a2183133f427bedb6d76896a0acf01f7ba
maharshigor <gor.maharshi@gmail.com>
2025-06-12 11:09:08 -0400

    ENH: Schedule looks correct.
    - Changes in the schedule_template.
    - Changes in the schedule directive nodes.

--- sphinxext/workshop/schedule.py
+++ sphinxext/workshop/schedule.py
@@ -1,6 +1,6 @@
 from docutils import nodes
-from sphinx.util.docutils import SphinxDirective
 from sphinx.util import logging
+from sphinx.util.docutils import SphinxDirective
 
 logger = logging.getLogger(__name__)
 
@@ -9,31 +9,31 @@ class ScheduleDirective(SphinxDirective):
     has_content = True
 
     def run(self):
-        section = nodes.section(ids=['schedule'])
-        section['classes'] += ['full-page', 'program', 'p-b-50'] # Outer section styling
-
-        # Add title directly to section
-        title = nodes.title(text="Schedule")
-        title['classes'] += ['content-title', 'l1-txt1', 'p-b-10', 'color-black']
-        section += title
+        section = nodes.section(ids=["schedule"])
+        section["classes"] += [
+            "p-b-50",
+        ]  # Outer section styling
 
         # Container for the timeline content rendered by nested directives
         content_node = nodes.container()
-        content_node['classes'] += ['full-page-content', 'program-content']
+        content_node["classes"] += ["full-page-content", "program-content"]
 
         # Add divider and timezone note at the top of the content area
         divider = nodes.container()
-        divider['classes'] += ['speaker-divider', 'm-b-10']
+        divider["classes"] += ["heading-underline", "m-b-10"]
         timezone_note = nodes.strong(
             text="All times mentioned are according to EST Time Zone.",
-            classes=['s2-txt2', 'fs-16', 'm-b-20']
+            classes=["s2-txt2", "fs-16"],
         )
         content_node += divider
         content_node += timezone_note
 
+        row = nodes.container()
+        row["classes"] += ["row"]
         # Parse the nested timeline directives, their output will be added to
         # content_node
-        self.state.nested_parse(self.content, self.content_offset, content_node)
+        self.state.nested_parse(self.content, self.content_offset, row)
+        content_node += row
         section += content_node
 
         return [section]
