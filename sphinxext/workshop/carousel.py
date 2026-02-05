def722043f2c038738b35d93c0bfb7f0b14389d7
deka27 <mailerdeka@gmail.com>
2026-02-05 14:56:56 -0500

    RF: Code format Fix by RUFF because of version update

--- sphinxext/workshop/carousel.py
+++ sphinxext/workshop/carousel.py
@@ -82,10 +82,8 @@ class CarouselDirective(SphinxDirective):
         try:
             jinja_loader = FileSystemLoader(template_dir)
             jinja_env = Environment(loader=jinja_loader, autoescape=True)
-            jinja_env.globals["pathto"] = (
-                lambda filename, *args: html_builder.get_relative_uri(
-                    env.docname, filename
-                )
+            jinja_env.globals["pathto"] = lambda filename, *args: (
+                html_builder.get_relative_uri(env.docname, filename)
             )
             template = jinja_env.get_template(template_basename)
             rendered_html = template.render(carousel_items=env.workshop_carousel)
